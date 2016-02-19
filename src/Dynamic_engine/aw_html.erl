e3471156cecc98f064ad95650f8eae67064cc9e6
kirilov <bogong@mail.ru>
2016-02-19 20:23:29 +0300

    Adding: aw_html:script, aw_html:title

--- src/Dynamic_engine/aw_html.erl
+++ src/Dynamic_engine/aw_html.erl
@@ -8,11 +8,13 @@
 %%%-------------------------------------------------------------------
 -module(aw_html).
 -author("Alexandr KIRILOV, http://alexandr.kirilov.me").
--vsn("0.0.1.240").
+-vsn("0.0.3.242").
 
 %% API
 -export([
-	link/2
+	link/2,
+	script/2,
+	title/1
 ]).
 
 %%-----------------------------------
@@ -20,11 +22,31 @@
 -spec link(Type,File_path) -> list()
 	when
 		Type :: atom(),
-		File_path :: unicode:latin1_charlist().
+		File_path :: unicode:chardata().
 
 link(css,File_path) when is_list(File_path) ->
-	case io_lib:char_list(File_path) of
-		true -> lists:concat(["<link href=\"",File_path,"\" rel=\"stylesheet\" type=\"text/css\">\n"]);
-		false -> "<link>Bad argument</link>\n"
-	end;
+	lists:concat(["<link href=\"",File_path,"\" rel=\"stylesheet\" type=\"text/css\">\n"]);
 link(_,_) -> "<link>Bad argument</link>\n".
+
+
+%%-----------------------------------
+%% @doc Return prepared for Yaws Appmod script tag
+-spec script(Type,Properties) -> list()
+	when
+		Type :: atom(),
+		Properties :: any().
+
+script(js,{file_link,Url,Charset}) when is_list(Url), is_list(Charset) ->
+	lists:concat(["<script type=\"text/javascript\" src=\"",Url,"\" charset=\"",Charset,"\"></script>\n"]);
+script(js,{script,Script}) when is_list(Script) ->
+	lists:concat(["<script>",Script,"</script>\n"]);
+script(_,_) -> "<script>Bad argument</script>".
+
+
+%%-----------------------------------
+%% @doc Return prepared for Yaws Appmod title tag
+-spec title(Title::unicode:chardata()) -> unicode:chardata().
+
+title(Title) when is_list(Title) ->
+	lists:concat(["<title>",Title,"</title>\n"]);
+title(_) -> "<title>Bad argument</title>\n".
