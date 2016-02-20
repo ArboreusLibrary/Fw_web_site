c8607c734fa7021491634b9160d6e84b261292a9
kirilov <bogong@mail.ru>
2016-02-20 10:28:04 +0300

    Adding: aw_html:tag_string/3, aw_html:single_tag_string/2, aw_html:li/2

--- src/Universal/aw_html.erl
+++ src/Universal/aw_html.erl
@@ -8,11 +8,13 @@
 %%%-------------------------------------------------------------------
 -module(aw_html).
 -author("Alexandr KIRILOV, http://alexandr.kirilov.me").
--vsn("0.0.5.244").
+-vsn("0.0.7.248").
 
 %% API
 -export([
+	tag_string/3,single_tag_string/2,
 	a/2,
+	li/2,
 	link/2,
 	script/2,
 	style/1,
@@ -21,22 +23,71 @@
 
 
 %%-----------------------------------
-%% @doc Return prepared a tag for Yaws appmode. In case of usage string mode proplist
+%% @doc Return a tag string. In case of usage string mode proplist
 %% should be {"String_attribute","String_value"}, in case of usage tuple mode
 %% proplist should be {atom_attribute,"String_value"}
+-spec tag_string(Tag_name,Attributes_proplist,Value) -> list()
+	when
+		Tag_name :: unicode:latin1_chardata(),
+		Attributes_proplist :: proplists:proplist(),
+		Value :: unicode:chardata().
+
+tag_string(Tag_name,[],Value) when is_list(Tag_name), is_list(Value) ->
+	lists:concat(["<",Tag_name,">",Value,"</",Tag_name,">"]);
+tag_string(Tag_name,Attributes_proplist,Value) when is_list(Tag_name), is_list(Value) ->
+	lists:concat([
+		"<",Tag_name," ",
+		[lists:concat([Attribute,"=\"",Value,"\" "])||{Attribute,Value} <- Attributes_proplist],">",
+		Value,"</",Tag_name,">"
+	]);
+tag_string(_,_,_) -> "<make_tag_string>Bag_argument</make_tag_string>".
+
+
+%%-----------------------------------
+%% @doc Return a tag string. In case of usage string mode proplist
+%% should be {"String_attribute","String_value"}, in case of usage tuple mode
+%% proplist should be {atom_attribute,"String_value"}
+-spec single_tag_string(Tag_name,Attributes_proplist) -> list()
+	when
+		Tag_name :: unicode:latin1_chardata(),
+		Attributes_proplist :: proplists:proplist().
+
+single_tag_string(Tag_name,[]) when is_list(Tag_name) ->
+	lists:concat(["<",Tag_name,">"]);
+single_tag_string(Tag_name,Attributes_proplist) when is_list(Tag_name) ->
+	lists:concat([
+		"<",Tag_name," ",
+		[lists:concat([Attribute,"=\"",Value,"\" "])||{Attribute,Value} <- Attributes_proplist],">"
+	]);
+single_tag_string(_,_) -> "<make_tag_string>Bag_argument</make_tag_string>".
+
+
+%%-----------------------------------
+%% @doc Return prepared the tag a for Yaws appmode
 -spec a(Output_type,{Attributes_proplist,Text}) -> list() | tuple()
 	when
 		Output_type :: string | tuple,
 		Attributes_proplist :: proplists:proplist(),
 		Text :: unicode:chardata().
 
-a(string,{Attributes_proplist,Text}) when is_list(Attributes_proplist), is_list(Text) ->
-	lists:concat([
-		"<a ",[lists:concat([Attribute,"=\"",Value,"\" "])||{Attribute,Value} <- Attributes_proplist],
-		">",Text,"</a>"]);
-a(tuple,{Attributes_proplist,Text}) when is_list(Attributes_proplist), is_list(Text) ->
-	{'a',Attributes_proplist,Text};
+a(string,{Attributes_proplist,Value}) -> tag_string("a",Attributes_proplist,Value);
+a(tuple,{Attributes_proplist,Value}) when is_list(Attributes_proplist), is_list(Value) ->
+	{'a',Attributes_proplist,Value};
 a(_,_) -> "<a>Bad argument</a>".
+
+
+%%-----------------------------------
+%% @doc Return prepared the tag li for Yaws appmode
+-spec li(Output_type,{Attributes_proplist,Text}) -> list() | tuple()
+	when
+		Output_type :: string | tuple,
+		Attributes_proplist :: proplists:proplist(),
+		Text :: unicode:chardata().
+
+li(string,{Attributes_proplist,Value}) -> tag_string("li",Attributes_proplist,Value);
+li(tuple,{Attributes_proplist,Value}) when is_list(Attributes_proplist), is_list(Value) ->
+	{'li',Attributes_proplist,Value};
+li(_,_) -> "<li>Bag argument</li>".
 
 
 %%-----------------------------------
