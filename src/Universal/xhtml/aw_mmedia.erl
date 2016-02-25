c6045801d1444eeeb315c759ea420c11ae0c70d7
kirilov <bogong@mail.ru>
2016-02-26 00:31:46 +0300

    Adding: aw_mmedia

---
+++ src/Universal/xhtml/aw_mmedia.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author Alexandr KIRILOV
+%%% @copyright (C) 2016, http://arboreus.system
+%%% @doc
+%%%
+%%% @end
+%%% Created : 26. Февр. 2016 0:31
+%%%-------------------------------------------------------------------
+-module(aw_mmedia).
+-author("Alexandr KIRILOV, http://alexandr.kirilov.me").
+
+%% API
+-export([]).
